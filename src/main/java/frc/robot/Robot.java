a6a32d02684071d0025f832c50cfc5fa80f9a2be
Perry Robotics <phsrambots@gmail.com>
2025-03-14 20:25:28 -0400

    More Auto Changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,26 +23,21 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run(); 
-
     if (kUseLimelight) {
       var driveState = m_robotContainer.drivetrain.getState();
       double headingDeg = driveState.Pose.getRotation().getDegrees();
       double omegaRps = Units.radiansToRotations(driveState.Speeds.omegaRadiansPerSecond);
-  
+
       LimelightHelpers.SetRobotOrientation("limelight", headingDeg, 0, 0, 0, 0, 0);
-      var llMeasurement = LimelightHelpers.getBotPoseEstimate_wpiRed("limelight");
+      var llMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
       if (llMeasurement != null && llMeasurement.tagCount > 0 && Math.abs(omegaRps) < 2.0) {
         m_robotContainer.drivetrain.addVisionMeasurement(llMeasurement.pose, llMeasurement.timestampSeconds);
       }
       if (Math.random() > .95) {
         System.out.println("headingDeg: " + headingDeg);
+      }
     }
-    }
-    // }
-      // if (Math.random() > .95) {
-      //   System.out.println("headingDeg: " + headingDeg);
-      // }
-    }
+  }
   
 
   @Override
